ef038c87cd55c878f4cc0469f550dbbd05d7ddd4
KushibikiMashu <kushibiki3413@gmail.com>
2018-10-12 09:49:04 +0900

    generate a json file order by published_at desc

--- laravel/app/Console/Commands/GenerateJson.php
+++ laravel/app/Console/Commands/GenerateJson.php
@@ -38,18 +38,26 @@ class GenerateJson extends Command
      */
     public function handle()
     {
-        $video = DB::table('video')->get();
+        $videos = DB::table('video')
+                    ->orderBy('published_at', 'desc')
+                    ->get();
+        $channels = DB::table('channel')
+                    ->get();
 
         $songs = [];
         $i = 0;
-        foreach($video as $song){
-            $songs[$i]['video_hash'] = $song->video_hash;
-            $songs[$i]['title'] = $song->title;
-            $songs[$i]['date']  = $song->published_at;
+
+        foreach($videos as $video){
+            $songs[$i]['hash'] = $video->video_hash;
+            $songs[$i]['title'] = $video->title;
+            $songs[$i]['date']  = $video->published_at;
+            $songs[$i]['channel']  = $channels[$video->channel_id - 1]->title;
+            $songs[$i]['img']  = 'https://i.ytimg.com/vi/' . $video->video_hash . '/hqdefault.jpg';
             $i++;
         }
 
         $json = json_encode($songs, JSON_UNESCAPED_UNICODE);
-        var_dump($json);
+        $file = dirname(__FILE__) . '/../../../public/json/songs.json';
+        file_put_contents($file, $json);
     }
 }
