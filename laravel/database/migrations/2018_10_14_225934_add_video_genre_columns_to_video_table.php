ec7623e520f8e097a8865724ad9a4f17316f0577
KushibikiMashu <kushibiki3413@gmail.com>
2018-10-15 00:10:44 +0900

    Add genre column to video table

---
+++ laravel/database/migrations/2018_10_14_225934_add_video_genre_columns_to_video_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddVideoGenreColumnsToVideoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('video', function (Blueprint $table) {
+            $table->string('genre')->after('video_hash');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('video', function (Blueprint $table) {
+            //
+        });
+    }
+}
