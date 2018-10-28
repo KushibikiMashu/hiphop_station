8fdc52a7d73db83e230ef64884e98de0e658dcdb
KushibikiMashu <kushibiki3413@gmail.com>
2018-10-28 19:15:49 +0900

    [FIX] Add sudo to each command

--- install.docker.sh
+++ install.docker.sh
@@ -4,18 +4,16 @@
 if which docker > /dev/null 2>&1; then
     echo 'Docker has already installed'
 else
-    sudo yum update -y \
-        && yum install -y docker \
-        && service docker start \
-        && usermod -a -G docker ec2-user
+    sudo yum update -y
+    sudo yum install -y docker
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
 fi
 
 # Install Docker-Compose
 if which docker-compose > /dev/null 2>&1; then
     echo 'Docker-Compose has already installed'
 else
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
-        && chmod +x /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 fi
-
-echo 'Exit EC2 and re-login to enable docker'
