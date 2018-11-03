6a663afbe7d9adaa05323888b0a689015c296428
KushibikiMashu <kushibiki3413@gmail.com>
2018-11-04 03:20:16 +0900

    [origin/feature/20181104_get_video_thumbnail_image]Fetch Video Thumbnail from YouTube

---
+++ laravel/app/Console/Commands/fetchVideoThumbnailImage.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
+use App\Video;
+use App\VideoThumbnail;
+
+class fetchVideoThumbnailImage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fetch:videoThumbnail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    protected $videoThumbnailQuery;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->videoThumbnailQuery = VideoThumbnail::get();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // DBからstd, medium, highのアドレスを取得する
+        // curl(Guzzle使う？)で画像を取得
+        // publicディレクトリ配下、サイズごとに配置する
+        // 画像名はhash.jpg
+        $data = VideoThumbnail::get();
+        $this->fetchThumbnailInDatabase($data);
+
+        // 将来的には...
+        // S3に動画をアップ
+        // S3のアドレスをjsonで管理
+        // jsonをフロント（React）に渡す
+    }
+
+    public function fetchNewVideoThumbnail()
+    {
+        //
+    }
+
+    public function fetchThumbnailInDatabase($data)
+    {
+        $sizes = ['std', 'medium', 'high'];
+        foreach ($data as $record) {
+            foreach ($sizes as $size) {
+                $this->getImages($record, $size);
+            }
+        }
+    }
+
+    private function getImages($record, string $size)
+    {
+        $url = $record->{$size};
+        $hash = substr(pathinfo($url)['dirname'], -11);
+        $path = public_path("image/video_thumbnail/{$size}/{$hash}.jpg");
+        $data = file_get_contents($url);
+        file_put_contents($path, $data);
+    }
+}
---
+++ laravel/app/VideoThumbnail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VideoThumbnail extends Model
+{
+    protected $table = 'video_thumbnail';
+
+}
