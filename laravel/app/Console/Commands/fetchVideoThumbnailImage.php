86497fcb4987ad07c7e0d5fe00a4a116fb62a24e
KushibikiMashu <kushibiki3413@gmail.com>
2018-11-04 14:12:28 +0900

    fixup! [origin/feature/20181104_get_video_thumbnail_image]Fetch Video Thumbnail from YouTube

--- laravel/app/Console/Commands/fetchVideoThumbnailImage.php
+++ laravel/app/Console/Commands/fetchVideoThumbnailImage.php
@@ -25,16 +25,18 @@ class fetchVideoThumbnailImage extends Command
     protected $description = 'Command description';
 
     protected $videoThumbnailQuery;
+    protected $videoThumbnailTableName;
 
     /**
      * Create a new command instance.
      *
-     * @return void
+     * @param VideoThumbnail $videoThumbnail
      */
-    public function __construct()
+    public function __construct(VideoThumbnail $videoThumbnail)
     {
         parent::__construct();
-        $this->videoThumbnailQuery = VideoThumbnail::get();
+        $this->videoThumbnailQuery = $videoThumbnail->get();
+        $this->videoThumbnailTableName = $videoThumbnail->getTable();
     }
 
     /**
@@ -48,18 +50,12 @@ class fetchVideoThumbnailImage extends Command
         // curl(Guzzle使う？)で画像を取得
         // publicディレクトリ配下、サイズごとに配置する
         // 画像名はhash.jpg
-        $data = VideoThumbnail::get();
-        $this->fetchThumbnailInDatabase($data);
+        $this->fetchThumbnailInDatabase($this->videoThumbnailQuery);
 
         // 将来的には...
         // S3に動画をアップ
         // S3のアドレスをjsonで管理
         // jsonをフロント（React）に渡す
-    }
-
-    public function fetchNewVideoThumbnail()
-    {
-        //
     }
 
     public function fetchThumbnailInDatabase($data)
@@ -74,13 +70,14 @@ class fetchVideoThumbnailImage extends Command
 
     private function getImages($record, string $size)
     {
-        $url = $record->{$size};
+        $url = str_replace('_live', '', $record->{$size});
         $hash = substr(pathinfo($url)['dirname'], -11);
-        $image_path = "image/video_thumbnail/{$size}/{$hash}.jpg";
-        if (!file_exists(public_path($image_path))) {
-            $path = public_path($image_path);
-            $data = file_get_contents($url);
-            if ($data) file_put_contents($path, $data);
-        }
+        $image_path = "image/{$this->videoThumbnailTableName}/{$size}/{$hash}.jpg";
+        if (file_exists(public_path($image_path))) return;
+
+        // レスポンスコードが400系・500系でもwarningを出さない
+        $context = stream_context_create(['http' => ['ignore_errors' => true]]);
+        $data = file_get_contents($url, false, $context);
+        if (strpos($http_response_header[0], '200') !== false) file_put_contents(public_path($image_path), $data);
     }
 }
