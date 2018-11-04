cabbb96849a3319fbaa90817e8bf0cd8a7c8a198
KushibikiMashu <kushibiki3413@gmail.com>
2018-11-04 15:46:08 +0900

    [feature/20181104_get_channel_thumbnail_image]Refactoring FetchVideoThumbnailImage class

---
+++ laravel/app/Console/Commands/FetchChannelThumbnailImage.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\ChannelThumbnail;
+use App\Console\Commands\Services\ThumbnailImageFetcher;
+use Illuminate\Console\Command;
+
+class FetchChannelThumbnailImage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fetch:channelThumbnail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @param ChannelThumbnail $channelThumbnail
+     */
+    public function handle(ChannelThumbnail $channelThumbnail)
+    {
+        $fetcher = new ThumbnailImageFetcher($channelThumbnail);
+        $fetcher->fetchThumbnailInDatabase();
+    }
+}
--- laravel/app/Console/Commands/FetchVideoThumbnailImage.php
+++ laravel/app/Console/Commands/FetchVideoThumbnailImage.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Console\Commands\Services\ThumbnailImageFetcher;
 use App\VideoThumbnail;
 
 class FetchVideoThumbnailImage extends Command
@@ -21,60 +22,31 @@ class FetchVideoThumbnailImage extends Command
      */
     protected $description = 'Command description';
 
-    protected $videoThumbnailQuery;
-    protected $videoThumbnailTableName;
-
     /**
      * Create a new command instance.
-     *
-     * @param VideoThumbnail $videoThumbnail
      */
-    public function __construct(VideoThumbnail $videoThumbnail)
+    public function __construct()
     {
         parent::__construct();
-        $this->videoThumbnailQuery = $videoThumbnail->get();
-        $this->videoThumbnailTableName = $videoThumbnail->getTable();
     }
 
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @param VideoThumbnail $videoThumbnail
      */
-    public function handle()
+    public function handle(VideoThumbnail $videoThumbnail)
     {
         // DBからstd, medium, highのアドレスを取得する
         // curl(Guzzle使う？)で画像を取得
         // publicディレクトリ配下、サイズごとに配置する
         // 画像名はhash.jpg
-        $this->fetchThumbnailInDatabase($this->videoThumbnailQuery);
+        $fetcher = new ThumbnailImageFetcher($videoThumbnail);
+        $fetcher->fetchThumbnailInDatabase();
 
         // 将来的には...
         // S3に動画をアップ
         // S3のアドレスをjsonで管理
         // jsonをフロント（React）に渡す
-    }
-
-    public function fetchThumbnailInDatabase($data)
-    {
-        $sizes = ['std', 'medium', 'high'];
-        foreach ($data as $record) {
-            foreach ($sizes as $size) {
-                $this->getImages($record, $size);
-            }
-        }
-    }
-
-    private function getImages($record, string $size)
-    {
-        $url = str_replace('_live', '', $record->{$size});
-        $hash = substr(pathinfo($url)['dirname'], -11);
-        $image_path = "image/{$this->videoThumbnailTableName}/{$size}/{$hash}.jpg";
-        if (file_exists(public_path($image_path))) return;
-
-        // レスポンスコードが400系・500系でもwarningを出さない
-        $context = stream_context_create(['http' => ['ignore_errors' => true]]);
-        $data = file_get_contents($url, false, $context);
-        if (strpos($http_response_header[0], '200') !== false) file_put_contents(public_path($image_path), $data);
     }
 }
