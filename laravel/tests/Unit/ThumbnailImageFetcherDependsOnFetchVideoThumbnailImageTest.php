7bc85decd1e439db8bd0c6ad8caf39c9c7f5f36a
KushibikiMashu <kushibiki3413@gmail.com>
2018-11-08 01:46:30 +0900

    fixup! [feature/20181107_add_test_code_on_phpunit]Simplify test of deleteInvalidRecord Method in ThumbnailImageFetcher class

--- laravel/tests/Unit/ThumbnailImageFetcherDependsOnFetchVideoThumbnailImageTest.php
+++ laravel/tests/Unit/ThumbnailImageFetcherDependsOnFetchVideoThumbnailImageTest.php
@@ -17,30 +17,28 @@ class ThumbnailImageFetcherDependsOnFetchVideoThumbnailImageTest extends TestCas
      * @var ThumbnailImageFetcher instance
      */
     private $instance;
-    private $dependencyInstance;
 
     public function setUp()
     {
         parent::setup();
-        $this->dependencyInstance = new VideoThumbnail;
-        $this->instance = new ThumbnailImageFetcher($this->dependencyInstance);
+        $this->instance = new ThumbnailImageFetcher(new VideoThumbnail);
     }
 
-    public function test_テーブル名を取得する()
+    public function test_テーブル名を取得する(): void
     {
         $this->assertSame('video_thumbnail', $this->instance::getTableName());
         $this->assertSame('video', $this->instance::getParentTableName());
         $this->assertSame(VideoThumbnail::get()[0]->high, $this->instance::getThumbnailQuery()[0]->high);
     }
 
-    public function test_video_thumbnailテーブルのクエリの同一性を担保する()
+    public function test_video_thumbnailテーブルのクエリの同一性を担保する(): void
     {
         $this->assertSame(VideoThumbnail::get()[0]->id, $this->instance::getThumbnailQuery()[0]->id);
         $this->assertSame(VideoThumbnail::get()[0]->video_id, $this->instance::getThumbnailQuery()[0]->video_id);
         $this->assertSame(VideoThumbnail::get()[0]->medium, $this->instance::getThumbnailQuery()[0]->medium);
     }
 
-    public function test_videoテーブルのクエリの同一性を担保する()
+    public function test_videoテーブルのクエリの同一性を担保する(): void
     {
         $this->assertSame(Video::get()[0]->id, $this->instance::getParentTableQuery()[0]->id);
         $this->assertSame(Video::get()[0]->channel_id, $this->instance::getParentTableQuery()[0]->channel_id);
@@ -50,7 +48,7 @@ class ThumbnailImageFetcherDependsOnFetchVideoThumbnailImageTest extends TestCas
         $this->assertSame(Video::get()[0]->published_at, $this->instance::getParentTableQuery()[0]->published_at);
     }
 
-    public function test_deleteInvalidRecord_YouTubeで削除済みの動画をDBから削除する()
+    public function test_deleteInvalidRecord_YouTubeで削除済みの動画をDBから削除する(): void
     {
         // TODO Fakerでデータ入れる
         $videoId = Video::insertGetId([
@@ -60,7 +58,6 @@ class ThumbnailImageFetcherDependsOnFetchVideoThumbnailImageTest extends TestCas
             'genre'        => 'song',
             'published_at' => '2000-01-01T00:00:00.000Z',
         ]);
-
         $videoThumbnailId = VideoThumbnail::insertGetId([
             'video_id' => $videoId,
             'std'      => 'std',
@@ -71,15 +68,13 @@ class ThumbnailImageFetcherDependsOnFetchVideoThumbnailImageTest extends TestCas
         // データが挿入されたことを確認
         $this->assertTrue(Video::where('id', '=', $videoId)->exists());
         $this->assertTrue(VideoThumbnail::where('id', '=', $videoThumbnailId)->exists());
-
         $method = $this->callPrivateMethod($this->instance, 'deleteInvalidRecord');
         $method->invoke($this->instance, 'video_thumbnail', $videoThumbnailId, 'abcdefg');
-
         $this->assertFalse(Video::where('id', '=', $videoId)->exists());
         $this->assertFalse(VideoThumbnail::where('id', '=', $videoThumbnailId)->exists());
     }
 
-    public function callPrivateMethod($class, $method)
+    public function callPrivateMethod(ThumbnailImageFetcher $class, string $method): \ReflectionMethod
     {
         $reflection = new \ReflectionClass($class);
         $method = $reflection->getMethod($method);
