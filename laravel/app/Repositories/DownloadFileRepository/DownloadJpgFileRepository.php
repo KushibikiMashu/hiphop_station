b7f33efa60c760aca7d28eb86bd34926475d8b0a
KushibikiMashu <kushibiki3413@gmail.com>
2018-11-11 09:14:12 +0900

    fixup! [feature/add_test_code_on_phpunit]👏Introduce Repository Pattern

--- laravel/app/Repositories/DownloadFileRepository/DownloadJpgFileRepository.php
+++ laravel/app/Repositories/DownloadFileRepository/DownloadJpgFileRepository.php
@@ -6,15 +6,13 @@ namespace App\Repositories;
 class DownloadJpgFileRepository implements DownloadFileRepositoryInterface
 {
     // 別のRepository作って、そこに実装してもいいと思う。OnlineImageRepositoryとか。外部からのデータ取得なので、オニオン型の一番外だから
-    public function canDownloadJpgFromUrl(string $url, string $image_path): bool
+    public function couldDownloadJpgFromUrl(string $url, string $file_path): bool
     {
         // レスポンスコードが400系・500系でもwarningを出さない
         $context = stream_context_create(['http' => ['ignore_errors' => true]]);
         $data = file_get_contents($url, false, $context);
-        if (strpos($http_response_header[0], '200') !== false) {
-            file_put_contents(public_path($image_path), $data);
-            return true;
-        }
-        return false;
+        if (strpos($http_response_header[0], '200') === false) return false;
+        file_put_contents(public_path($file_path), $data);
+        return true;
     }
 }
