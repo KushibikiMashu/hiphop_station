aa64dcd5dcb701029a67917d5e3a0c8a24c2f58c
KushibikiMashu <kushibiki3413@gmail.com>
2018-11-18 02:24:52 +0900

    fixup! [feature/add_test_code_on_phpunit]Separate FetchLatestVideosFromYoutubeApi class to Repository and Service class

--- laravel/tests/Feature/ForSimpleTest.php
+++ laravel/tests/Feature/ForSimpleTest.php
@@ -1,23 +1,31 @@
 <?php
-//
-//namespace Tests\Feature;
-//
-//use Tests\TestCase;
-//use Illuminate\Foundation\Testing\WithFaker;
-//use Illuminate\Foundation\Testing\RefreshDatabase;
-//use App\Repositories\VideoRepository;
-//use App\Console\Commands\FetchLatestVideosFromYoutubeApi;
-//
-//
-//class ForSimpleTest extends TestCase
-//{
-//    private $instance;
-//    private $table = 'video';
-//
-//    public function setUp(): void
-//    {
-//        parent::setup();
-//        $this->instance = new VideoRepository;
-//    }
-//
-//}
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Repositories\VideoRepository;
+use App\Console\Commands\FetchLatestVideosFromYoutubeApi;
+
+
+class ForSimpleTest extends TestCase
+{
+    private $instance;
+    private $table = 'video';
+
+    public function setUp(): void
+    {
+        parent::setup();
+        $this->instance = new VideoRepository;
+    }
+
+    /**
+     * @test
+     */
+    public function simpleTest()
+    {
+        $this->assertTrue(True);
+    }
+
+}
--- laravel/tests/Unit/ApiRepositoryTest.php
+++ laravel/tests/Unit/ApiRepositoryTest.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Repositories\ApiRepository;
 use App\Repositories\VideoRepository;
 use App\Repositories\ChannelRepository;
+use App\Services\CustomizedYoutubeApi;
 
 class ApiRepositoryTest extends TestCase
 {
@@ -16,11 +17,15 @@ class ApiRepositoryTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-        $this->instance = new ApiRepository(new VideoRepository, new ChannelRepository);
+        $this->instance = new ApiRepository(new VideoRepository, new ChannelRepository, new CustomizedYoutubeApi);
     }
 
+    /**
+     * @test
+     */
     public function newVideosOfRegisteredChannel__最新の動画を取得する()
     {
+        $this->assertTrue(True);
         // 作れるのか？
     }
 }
