20088e2398d9ebcd8ab955b81e387ff700b28c67
KushibikiMashu <kushibiki3413@gmail.com>
2018-12-02 01:18:01 +0900

    [arrange_components_of_resource_directory]Update site description

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
-        <title>HIPSTY | 日本語ラップの動画サイト</title>
+        <title>HIPSTY | 日本語ラップ好きのための動画サイト</title>
     </head>
     <body>
         <div id="root"></div>
