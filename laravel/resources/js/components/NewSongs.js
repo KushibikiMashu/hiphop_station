29c7438cc559b22623e15bf2c3f3584729367409
KushibikiMashu <kushibiki3413@gmail.com>
2018-12-01 16:24:34 +0900

    [arrange_components_of_resource_directory]Curve out Dummy Components

--- laravel/resources/js/components/NewSongs.js
+++ laravel/resources/js/components/NewSongs.js
@@ -1,140 +1,14 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import {Link} from 'react-router-dom';
 import request from 'superagent';
 import {pathToJson} from './const';
-import LinearProgress from '@material-ui/core/LinearProgress';
 import VideoCard from './organisms/VideoCard';
-import LabelBottomNavigation from "./LabelBottomNavigation";
+import VideoCardDummy from './organisms/VideoCardDummy';
+import VideoList from './templates/VideoList';
+import VideoListDummy from './templates/VideoListDummy';
 
 const PATH = pathToJson("main");
-
-const styles = theme => ({
-    flex: {
-        flexGrow: 1,
-    },
-    card: {
-        maxWidth: 260,
-    },
-    media: {
-        height: 0,
-        paddingTop: '56.25%', // 16:9
-    },
-    headline: {
-        textAlign: 'center',
-        marginTop: -8,
-        paddingBottom: 12,
-    },
-    cardContent: {
-        paddingTop: 8,
-        paddingBottom: 4,
-        paddingLeft: 12,
-        paddingRight: 12,
-    },
-    button: {
-        marginTop: 12,
-        marginBottom: 44,
-    },
-    flexDummy: {
-        maxHeight: 640
-    },
-    cardDummy: {
-        maxWidth: 260,
-    },
-    cardContentDummy: {
-        height: 80,
-        paddingTop: 8,
-        paddingBottom: 4,
-        paddingLeft: 12,
-        paddingRight: 12,
-    },
-    mediaDummy: {
-        height: 0,
-        width: 260,
-        backgroundColor: '#E0E0E0',
-        paddingTop: '56.25%', // 16:9
-    },
-    progressLong: {
-        height: 16,
-        width: 140,
-        marginLeft: 8,
-        marginTop: 12,
-        borderRadius: 20,
-        backgroundColor: '#BDBDBD',
-    },
-    progressShort: {
-        height: 16,
-        width: 100,
-        marginLeft: 8,
-        marginTop: 12,
-        borderRadius: 20,
-        backgroundColor: '#BDBDBD',
-    },
-    diffDate: {
-        marginLeft: 'auto'
-    }
-});
-
-class VideoList extends Component {
-    render() {
-        return <div className={this.props.classes.flex}>
-            <Grid container justify='center' direction="row" spacing={16}>
-                {this.props.videos}
-            </Grid>
-            <Grid container justify='center' direction="row">
-                <Button variant="extendedFab" aria-label="Load" className={this.props.classes.button}
-                        onClick={this.props.onClick}>
-                    LOAD MORE
-                </Button>
-            </Grid>
-        </div>;
-    }
-}
-
-VideoList.propTypes = {
-    classes: PropTypes.any,
-    videos: PropTypes.arrayOf(PropTypes.any),
-    onClick: PropTypes.func
-};
-
-function DummyVideoCard(props) {
-    return <Grid item>
-        <Card className={props.classes.cardDummy}>
-            <CardMedia
-                className={props.classes.mediaDummy} // 黒にする
-            />
-            <CardContent className={props.classes.cardContentDummy}>
-                <LinearProgress className={props.classes.progressLong}/>
-                <LinearProgress className={props.classes.progressShort}/>
-            </CardContent>
-            <CardActions>
-            </CardActions>
-        </Card>
-    </Grid>;
-}
-
-DummyVideoCard.propTypes = {classes: PropTypes.any};
-
-function DummyVideoList(props) {
-    return <div className={props.classes.flexDummy}>
-        <Grid container justify='center' direction="row" spacing={16}>
-            {props.videos}
-        </Grid>
-    </div>;
-}
-
-DummyVideoList.propTypes = {
-    classes: PropTypes.any,
-    videos: PropTypes.arrayOf(PropTypes.any)
-};
 
 class NewSongs extends React.Component {
     constructor(props) {
@@ -178,7 +52,6 @@ class NewSongs extends React.Component {
 
     // loadVideos関数が呼ばれると、再度render関数が作動する
     render() {
-        const {classes} = this.props;
         var videos = [];
 
         // asyncでres.bodyがstateに登録されるようにする
@@ -186,12 +59,12 @@ class NewSongs extends React.Component {
 
             for (var i = 0; i < 10; i++) {
                 videos.push(
-                    <DummyVideoCard key={i} classes={classes}/>
+                    <VideoCardDummy key={i}/>
                 )
             }
 
             return (
-                <DummyVideoList classes={classes} videos={videos}/>
+                <VideoListDummy videos={videos}/>
             );
         }
 
@@ -204,9 +77,7 @@ class NewSongs extends React.Component {
         }
 
         return (
-            <VideoList classes={classes} videos={videos} onClick={() => {
-                this.loadVideos()
-            }}/>
+            <VideoList videos={videos} onClick={() => {this.loadVideos()}}/>
         );
     }
 }
