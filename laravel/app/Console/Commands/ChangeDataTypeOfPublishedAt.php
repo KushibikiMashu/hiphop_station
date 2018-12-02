43f25f18ab46a194010388b3dcf6b0cea71b238e
KushibikiMashu <kushibiki3413@gmail.com>
2018-12-02 20:34:39 +0900

    [change_data_type_of_columns]Add published_at column of datetime data type

---
+++ laravel/app/Console/Commands/ChangeDataTypeOfPublishedAt.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ChangeDataTypeOfPublishedAt extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'change:publishedAt';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $channels = \App\Channel::all();
+        foreach ($channels as $channel) {
+            $channel->published_at = (new \Carbon\Carbon($channel->published_at_original))->format('Y-m-d H:i:s');
+            $channel->save();
+        }
+        $this->info('Channel変更完了');
+
+        $videos = \App\Video::all();
+        foreach ($videos as $video) {
+            $video->published_at = (new \Carbon\Carbon($video->published_at_original))->format('Y-m-d H:i:s');
+            $video->save();
+            $this->info($video->id);
+        }
+        $this->info('Video変更完了');
+    }
+}
---
+++ laravel/database/migrations/2018_12_02_200939_add_published_at.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPublishedAt extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('channel', function (Blueprint $table) {
+            $table->dateTime('published_at')
+                ->after('video_count')
+                ->default(\Carbon\Carbon::now()->format('Y-m-d H:i:s'));
+        });
+
+        Schema::table('video', function (Blueprint $table) {
+            $table->dateTime('published_at')
+                ->after('genre')
+                ->default(\Carbon\Carbon::now()->format('Y-m-d H:i:s'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('channel', function (Blueprint $table) {
+            $table->dropColumn('publishe    d_at');
+        });
+
+        Schema::table('video', function (Blueprint $table) {
+            $table->dropColumn('published_at');
+        });
+    }
+}
