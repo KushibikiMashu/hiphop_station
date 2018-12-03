3f8899906cbae1326f3c46c486ff0c2c9d4b919e
KushibikiMashu <kushibiki3413@gmail.com>
2018-12-04 02:18:03 +0900

    [update_video_genre]Exclude specific channel from LP

--- laravel/resources/js/components/const.js
+++ laravel/resources/js/components/const.js
@@ -1,8 +1,10 @@
 const pathToJson = (filename) => {
-    return location.origin + "/json/" + filename + ".json";
+    return location.origin + "/json/" + filename + ".json"
 }
 
-const newList = location.origin + '/api/video/new';
-const videoList = location.origin + '/api/video/list';
+const newList = location.origin + '/api/video/new'
+const videoList = location.origin + '/api/video/list'
 
-export { pathToJson, newList, videoList };
+const channelId = {"neetTokyo": 63}
+
+export { pathToJson, newList, videoList, channelId }
--- laravel/resources/js/components/pages/Routing.jsx
+++ laravel/resources/js/components/pages/Routing.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from 'react'
+import { Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import VideoList from "./VideoList";
+import { channelId } from '../const'
+import VideoList from "./VideoList"
 import VideoPlayerTemplate from '../templates/VideoPlayerTemplate'
 
 export default function Routing(props) {
     const {videos} = props
-    var MV = [],
+    var main = [],
+        MV = [],
         battle = [],
         interview = [],
         otheres = []
@@ -29,20 +31,23 @@ export default function Routing(props) {
             default:
                 break
         }
+        if (video.channelId !== channelId.neetTokyo) {
+            main.push(video)
+        }
     })
 
     return (
         <React.Fragment>
-            <Route exact path='/' render={() => <VideoList videos={videos}/>}/>
+            <Route exact path='/' render={() => <VideoList videos={main}/>}/>
             <Route path='/music_video' render={() => <VideoList videos={MV}/>}/>
             <Route path='/battle' render={() => <VideoList videos={battle}/>}/>
             <Route path='/interview' render={() => <VideoList videos={interview}/>}/>
             <Route path='/others' render={() => <VideoList videos={otheres}/>}/>
             <Route path='/video/:hash' render={() => <VideoPlayerTemplate videos={videos}/>}/>
         </React.Fragment>
-    );
+    )
 }
 
 Routing.propTypes = {
     videos: PropTypes.array.isRequired,
-};
+}
