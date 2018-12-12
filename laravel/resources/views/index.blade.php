862119a53dd7dcc425dcb76468769a3773d6c12e
KushibikiMashu <kushibiki3413@gmail.com>
2018-12-12 21:42:25 +0900

    [tweak_before_release]Load app.js through https

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -8,6 +8,6 @@
     </head>
     <body>
         <div id="root"></div>
-        <script src="{{ asset('/js/app.js') }}"></script>
+        <script src="{{ asset('/js/app.js', true) }}"></script>
     </body>
 </html>
